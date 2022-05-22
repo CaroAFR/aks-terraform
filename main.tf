ea93d203cd3284e4ac4568bd5ea6196c3a060999
CaroAFR <60991446+CaroAFR@users.noreply.github.com>
2022-05-22 13:28:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,14 +54,5 @@ resource "azurerm_kubernetes_cluster" "AKS-demo" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.AKS-demo.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value     = azurerm_kubernetes_cluster.AKS-demo.kube_config_raw
-  sensitive = true
-}
 
 
